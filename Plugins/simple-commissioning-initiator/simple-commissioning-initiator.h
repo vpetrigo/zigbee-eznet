6cab4ed5de7fe87d51aea99cf62fc27acf0dcaf7
Vladimir Petrigo <taenaru@gmail.com>
2016-06-23 15:00:00 +0300

    start splitting decribed in #10

---
+++ Plugins/simple-commissioning-initiator/simple-commissioning-initiator.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_COMMISSIONING_PLUGIN_H
+#define SIMPLE_COMMISSIONING_PLUGIN_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "../../include/af.h"
+
+/*! Commisioning start functions */
+EmberStatus SimpleCommissioningStart(uint8_t endpoint, 
+                                     bool is_server,
+                                     const uint16_t *clusters, 
+                                     uint8_t length);
+
+#endif // SIMPLE_COMMISSIONING_PLUGIN_H
